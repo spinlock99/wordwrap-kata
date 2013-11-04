9851b59bc1ae361d526d1d46f349fb4b34f28eca
Andrew Dixon <andrew.dixon@twitch.tv>
2013-11-03 18:53:35 -0800

    WordWrap in 3:11:22 including a party and stuff

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,2 +1,62 @@
 class Wordwrap
+  def initialize(wrap_length)
+    if !wrap_length.class.ancestors.find { |ancestor| ancestor == Integer }
+      raise "Wordwrap must be initialized with an integer"
+    end
+    @wrap_length = wrap_length
+  end
+
+  def wrap(words)
+    if (words.class != String)
+      raise "wrap must be called with a string parameter"
+    end
+
+    @words = words.split(/\s+/)
+
+    if (!@words.any? || @words.first.length < @wrap_length)
+      @wrapped_string = @words.shift
+      @wrapped_string ||= ""
+      character_count = @wrapped_string.length
+    else
+      @wrapped_string = String.new()
+      character_count = wrap_word(@words.shift, 0)
+    end
+
+    @words.each do |word|
+      puts "character_count: #{character_count}"
+      puts "word length: #{word.length}"
+
+      if (word.length > @wrap_length)
+        character_count = wrap_word(word, character_count)
+      elsif ((word.length + 1 + character_count) < @wrap_length)
+        @wrapped_string << " " << word
+        character_count += word.length
+      else
+        @wrapped_string << "\n" << word
+        character_count = 0
+      end
+    end
+
+    if (@wrapped_string[-1,1] == "\n")
+      @wrapped_string.chomp!
+    end
+
+    @wrapped_string
+  end
+
+  private
+  def wrap_word(string, character_count)
+    split_word = string.split("")
+    split_word.each do |character|
+      if ((character_count + 1) < @wrap_length)
+        @wrapped_string << character
+        character_count += 1
+      else
+        @wrapped_string << character << "\n"
+        character_count = 0
+      end
+    end
+    @wrapped_string << "\n"
+    character_count = 0
+  end
 end
--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -3,10 +3,76 @@ require 'wordwrap'
 
 describe Wordwrap do
   describe "#initialize" do
-    it "instantiates" do
-      expect {
-        Wordwrap.new
-      }.to_not raise_exception
+    shared_examples "raise exception" do
+      specify { expect { subject }.to raise_exception }
+    end
+
+    context "with wrap as an integer" do
+      subject(:wordwrap) { Wordwrap.new(5) }
+      it "instantiates" do
+        expect { wordwrap }.to_not raise_exception
+      end
+      it { should respond_to(:wrap) }
+    end
+    context "with no argument" do
+      subject(:wordwrap) { Wordwrap.new() }
+
+      it_behaves_like "raise exception"
+    end
+    context "with a string argument" do
+      subject(:wordwrap) { Wordwrap.new("foo") }
+
+      it_behaves_like "raise exception"
+    end
+  end
+
+  describe ".wrap" do
+    context "wrap at 5 characters" do
+      subject(:wordwrap) { Wordwrap.new(5) }
+
+      context "with a string parameter" do
+        let(:wrap) { wordwrap.wrap("word word") }
+
+        specify { expect { wrap }.to_not raise_exception }
+        it { wrap.should eq("word\nword") }
+      end
+      context "with a longer string parameter" do
+        let(:wrap) { wordwrap.wrap("word word word") }
+
+        it { wrap.should eq("word\nword\nword") }
+      end
+      context "with an array parameter" do
+        let(:wrap) { wordwrap.wrap([1,2,3,4]) }
+
+        it { expect { wrap }.to raise_exception }
+      end
+      context "with an empty string parameter" do
+        let(:wrap) { wordwrap.wrap("") }
+
+        it { wrap.should eq("") }
+      end
+      context "with a one word string parameter" do
+        let(:wrap) { wordwrap.wrap("word") }
+
+        it { wrap.should eq("word") }
+      end
+      context "with a longword" do
+        let(:wrap) { wordwrap.wrap("wordword") }
+
+        it { wrap.should eq("wordw\nord") }
+      end
+    end
+    context "wrap at 10 characters" do
+      subject(:wordwrap) { Wordwrap.new(10) }
+      let(:wrap) { wordwrap.wrap("word word word") }
+
+      it { wrap.should eq("word word\nword") }
+    end
+    context "wrap at 3 characters" do
+      subject(:wordwrap) { Wordwrap.new(3) }
+      let(:wrap) { wordwrap.wrap("word word word") }
+
+      it { wrap.should eq("wor\nd\nwor\nd\nwor\nd") }
     end
   end
 end
